9de655356f44a5e5cd47f74382f557529dbd89a4
Simon Pedersen <Simon Pedersen>
2016-08-14 12:44:54 +0200

    first

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var PBI_CV_EED04B38_70C9_4723_9C08_538DDC65A5FD_DEBUG = {
+        name: 'PBI_CV_EED04B38_70C9_4723_9C08_538DDC65A5FD_DEBUG',
+        displayName: 'SJKP.PbiViz.HellowWorld',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.1.0',
+        create: (options) => new powerbi.extensibility.visual.PBI_CV_EED04B38_70C9_4723_9C08_538DDC65A5FD.Visual(options),
+        custom: true
+    };
+}
